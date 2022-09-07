6f69856d62c871ff23e34e9cdbad5c2b6cb23e93
yotamos5699 <m.lhafakot@gmail.com>
2022-09-07 10:39:21 +0300

    - updated urls

--- src/Helper.js
+++ src/Helper.js
@@ -1,17 +1,17 @@
 const axios = require("axios");
-const dbUrl = "http://localhost:5000";
-
+const DBport =
+  process.env.DBport || "https://bizmod-db-server.herokuapp.com/" ||  "http://localhost:5000";
 const checkLoginAndReturnData = async (userObj, reqUrl) => {
   let options = {
-    url: `${dbUrl}${reqUrl}`,
+    url: `${DBport}${reqUrl}`,
     method: "POST",
-    mode:"no-cors",
+    mode: "no-cors",
     headers: {
       "Content-Type": "application/json;charset=UTF-8",
     },
     data: userObj,
   };
-
+  console.log(options);
   return axios(options).then((result) => {
     console.log("result  df", result.data);
     return result.data;
